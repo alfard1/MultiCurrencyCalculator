06e2970a78d8890a16a636f5e57360b02035291b
Pawel Wysokinski <pawel.wysokinski@gmail.com>
2019-08-31 11:40:10 +0200

    initial commit

---
+++ src/main/java/pl/edu/neg/MultiCurrencyCalculator/MultiCurrencyCalculatorApp.java
@@ -0,0 +1,13 @@
+package pl.edu.neg.MultiCurrencyCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiCurrencyCalculatorApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiCurrencyCalculatorApp.class, args);
+	}
+
+}
